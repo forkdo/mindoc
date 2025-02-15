34b9610cdb3df76e368ad61d6c6f71833ba68d4b
Flydo <jetsung@foxmail.com>
2025-02-15 16:15:23 +0000

    feat: 优化 Dockerfile,支持 GitHub Action

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/mindoc-org/mindoc
 
-go 1.18
+go 1.24.0
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
--- start.sh
+++ start.sh
@@ -1,31 +1,29 @@
-#!/bin/bash
-set -eux
+#!/usr/bin/env bash
 
-# 默认资源
-if [ ! -d "/mindoc/conf" ]; then mkdir -p "/mindoc/conf" ; fi
-if [[ -z "$(ls -A -- "/mindoc/conf")" ]] ; then cp -r "/mindoc/__default_assets__/conf" "/mindoc/" ; fi
+set -euo pipefail
 
-if [ ! -d "/mindoc/static" ]; then mkdir -p "/mindoc/static" ; fi
-if [[ -z "$(ls -A -- "/mindoc/static")" ]] ; then cp -r "/mindoc/__default_assets__/static" "/mindoc/" ; fi
+# 资源路径列表
+declare -A resources=(
+    [conf]="/mindoc/conf"
+    [static]="/mindoc/static"
+    [views]="/mindoc/views"
+    [uploads]="/mindoc/uploads"
+)
 
-if [ ! -d "/mindoc/views" ]; then mkdir -p "/mindoc/views" ; fi
-if [[ -z "$(ls -A -- "/mindoc/views")" ]] ; then cp -r "/mindoc/__default_assets__/views" "/mindoc/" ; fi
+# 初始化默认资源
+for key in "${!resources[@]}"; do
+    dir="${resources[$key]}"
+    default_dir="/mindoc/__default_assets__/$key"
 
-if [ ! -d "/mindoc/uploads" ]; then mkdir -p "/mindoc/uploads" ; fi
-if [[ -z "$(ls -A -- "/mindoc/uploads")" ]] ; then cp -r "/mindoc/__default_assets__/uploads" "/mindoc/" ; fi
+    mkdir -p "$dir"  # 确保目录存在
+    [[ -z "$(ls -A -- "$dir")" ]] && cp -r "$default_dir" "/mindoc/"
+done
 
-# 如果配置文件不存在就复制
+# 如果配置文件不存在则复制
 cp --no-clobber /mindoc/conf/app.conf.example /mindoc/conf/app.conf
 
-# 数据库等初始化
+# 数据库初始化
 /mindoc/mindoc_linux_amd64 install
 
-# 运行
-/mindoc/mindoc_linux_amd64
-
-# # Debug Dockerfile
-# while [ 1 ]
-# do
-#     echo "log ..."
-#     sleep 5s
-# done
+# 运行 Mindoc
+exec /mindoc/mindoc_linux_amd64
